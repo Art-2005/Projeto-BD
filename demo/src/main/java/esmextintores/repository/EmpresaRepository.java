966ed3b9bc4e8a1f17796d405db66d144856c966
Artur Souto <artursmpm@gmail.com>
2025-04-21 14:27:25 -0300

    ajeitado

--- demo/src/main/java/esmextintores/repository/EmpresaRepository.java
+++
@@ -1,7 +0,0 @@
-package esmextintores.repository;
-
-import esmextintores.model.Empresa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpresaRepository extends JpaRepository<Empresa, String> {
-}
